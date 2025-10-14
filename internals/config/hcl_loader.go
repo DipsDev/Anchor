cd9c315b33d99be0700a503e224d0b509685ec73
Ido Geva <ido.geva.b@gmail.com>
2025-10-14 15:03:44 +0300

    replace hclsimple with a more dynamic parsing functions

--- internals/config/hcl_loader.go
+++ internals/config/hcl_loader.go
@@ -1,18 +1,36 @@
 package config
 
-import "github.com/hashicorp/hcl/v2/hclsimple"
+import (
+	"errors"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"os"
+)
 
 type HclLoader struct {
 }
 
 func (l *HclLoader) Load(path string) (*Config, error) {
-	var config *Config
-	err := hclsimple.DecodeFile(path, nil, &config)
+	var config Config
+
+	data, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 
-	return config, nil
+	parser := hclparse.NewParser()
+
+	f, diags := parser.ParseHCL(data, path)
+	if diags != nil && diags.HasErrors() {
+		return nil, errors.New(diags.Error())
+	}
+
+	diags = gohcl.DecodeBody(f.Body, nil, &config)
+	if diags.HasErrors() {
+		return nil, errors.New(diags.Error())
+	}
+
+	return &config, nil
 }
 
 func NewHclLoader() *HclLoader {
