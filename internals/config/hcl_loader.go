9feefa19f681910c687b66ffcc3b51d847f78c0d
Ido Geva <ido.geva.b@gmail.com>
2025-10-14 12:44:54 +0300

    create simple hcl loader

---
+++ internals/config/hcl_loader.go
@@ -0,0 +1,16 @@
+package config
+
+import "github.com/hashicorp/hcl/v2/hclsimple"
+
+type HclLoader struct {
+}
+
+func (l *HclLoader) Load(path string) (*Config, error) {
+	var config *Config
+	err := hclsimple.DecodeFile(path, nil, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
