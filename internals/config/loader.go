21c0560311003b3d7ed23d4b52e19540db839216
Ido Geva <ido.geva.b@gmail.com>
2025-10-14 19:12:56 +0300

    rename config structs to know that the structs are the config types

--- internals/config/loader.go
+++ internals/config/loader.go
@@ -1,35 +1,35 @@
 package config
 
 type Config struct {
-	Environments []Environment `hcl:"environment,block"`
+	Environments []EnvironmentConfig `hcl:"environment,block"`
 }
-type Environment struct {
+type EnvironmentConfig struct {
 	Name        string `hcl:"name,label"`
 	Description string `hcl:"description"`
 
 	// Processes
-	Services []Service `hcl:"service,block"`
-	Tasks    []Task    `hcl:"task,block"`
+	Services []ServiceConfig `hcl:"service,block"`
+	Tasks    []TaskConfig    `hcl:"task,block"`
 }
 
-type Service struct {
-	Name        string      `hcl:"name,label"`
-	Engine      string      `hcl:"engine"`
-	HealthCheck HealthCheck `hcl:"health_check,block"`
-	DependsOn   []string    `hcl:"depends_on,optional"`
+type ServiceConfig struct {
+	Name        string            `hcl:"name,label"`
+	Engine      string            `hcl:"engine"`
+	HealthCheck HealthCheckConfig `hcl:"health_check,block"`
+	DependsOn   []string          `hcl:"depends_on,optional"`
 
 	// Engine dependent
 	Image   *string `hcl:"image"`
 	Command *string `hcl:"command"`
 }
 
-type Task struct {
+type TaskConfig struct {
 	Name      string   `hcl:"name,label"`
 	Command   string   `hcl:"command"`
 	DependsOn []string `hcl:"depends_on,optional"`
 }
 
-type HealthCheck struct {
+type HealthCheckConfig struct {
 	ConnectionType string `hcl:"type"`
 	Target         string `hcl:"target"`
 
