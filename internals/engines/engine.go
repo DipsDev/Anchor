c2aa060ab149ad9397264ee8c007dda4077ad0c1
DevDips <ido.geva.b@gmail.com>
2025-11-25 20:47:19 +0200

    create engine factory

--- internals/engines/engine.go
+++ internals/engines/engine.go
@@ -3,7 +3,10 @@ package engines
 type EngineExecutionResult string
 
 type AnchorEngine[T any] interface {
-	Parse(service &ServiceConfig) T
-	Execute() void
+	Parse(service &ServiceConfig) (T, error)
+	Execute() error
 }
 
+func Create(engineType string) (AnchorEngine, error) {
+	return nil, fmt.Errorf("wrong engine type provided, go engine '%v'", engineType)
+}
