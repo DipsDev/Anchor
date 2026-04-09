53460771fd4af5c0c494f2fef6a61b7d166e726c
Ido Geva <ido.geva.b@gmail.com>
2026-04-09 23:11:44 +0300

    start anchor from scratch

--- internals/runtime/cmd.go
+++
@@ -1,9 +0,0 @@
-package runtime
-
-func ApplyEnvironmentCmd(applyConfig EnvironmentStatusOptions) error {
-	return modifyEnvironment(applyEnvironment, applyConfig)
-}
-
-func StopEnvironmentCmd(stopConfig EnvironmentStatusOptions) error {
-	return modifyEnvironment(stopEnvironment, stopConfig)
-}
